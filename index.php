67f359a7f65162acb505d56206489079f2172326
Jakob C <jakob.chrois@gmail.com>
2018-09-22 15:51:16 +0200

    remove parts

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 		
 		<?php get_template_part('parts/part', 'introduction'); ?>
 		
-		<?php get_template_part('parts/part', 'services'); ?>
+		<?php //get_template_part('parts/part', 'services'); ?>
 
 		<?php //get_template_part('parts/part', 'about'); ?>
 
-		<?php get_template_part('parts/part', 'projects'); ?>
+		<?php //get_template_part('parts/part', 'projects'); ?>
 
-		<?php get_template_part('parts/part', 'contact'); ?>
+		<?php //get_template_part('parts/part', 'contact'); ?>
 
 <?php get_footer(); ?>
